018a22fd4a587f727999c67b61e777ce92ababf0
chaikivskti.t <chaikivskyi.t@gmail.com>
2025-09-15 21:29:17 +0200

    implement task

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,22 +2,40 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
+enum Status {
+  Open = "open",
+  Close = "close",
 }
 
+type Details = {
+  createAt: Date;
+  updateAt: Date;
+};
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: Status;
+  details?: Details;
+};
+
+const page1 = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
 const page2 = {
-  title: 'Python or Js',
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,9 +3,9 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
+function getPromise(): Promise<(string | number)[]> {
   return new Promise((resolve) => {
-    resolve(['Text', 50]);
+    resolve(["Text", 50]);
   });
 }
 
